f64a409822fae990d3df9052275e0ecff375c115
Ahmed Mohamed <ahmedabozied699@gmail.co>
2022-12-13 02:07:53 +0200

    add usePrev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import axios from "axios";
+import usePrevState from "./hooks/usePrevState";
 function App() {
+  const [term, setTerm] = useState("java");
+  const [result, setResult] = useState([]);
+  const prevTerm = usePrevState(term);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          srsearch: term,
+          format: "json",
+        },
+      });
+      setResult(data.query.search);
+    };
+    if (!result.length) {
+      search();
+    } else if (term !== prevTerm) {
+      const timeOut = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1200);
+      return () => {
+        clearTimeout(timeOut);
+      };
+    }
+  }, [term, result.length, prevTerm]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form className="d-flex m-5">
+        <Form.Control
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+
+      {/* ***useRef() */}
+      {/* <div>
+
+     <p>current term : {term}</p>
+      <p>pervious term :{pervTerm}</p>
+     </div> */}
+      <Table striped bordered hover className="m-5">
+        <thead>
+          <tr>
+            <th>title</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {result.map((data, index) => {
+            return (
+              <tr key={index}>
+                <td>{data.title}</td>
+                <td>
+                  <span dangerouslySetInnerHTML={{ __html: data.snippet }} />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
     </div>
   );
 }
